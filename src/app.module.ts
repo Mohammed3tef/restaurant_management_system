b019827fb45c4680210aeee2c3d2df328ef1da67
Mohamed Eldegwy <mohamed.eldegwy@3ddx.com>
2024-08-09 06:07:39 +0300

    Create docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { OrderModule } from './order/order.module';
 import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost:27017/restaurant_management_system',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the ConfigModule available globally
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     OrderModule,
     CustomerModule,
     ProductModule,
